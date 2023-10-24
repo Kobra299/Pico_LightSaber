fccf43711cc2cbe4897d5666fa270c1463ed3961
Wayne Norman <4283156+Kobra299@users.noreply.github.com>
2023-10-24 22:49:06 +0100

    Update Lightsaber.py

--- Lightsaber.py
+++ Lightsaber.py
@@ -1,31 +1,21 @@
-import json
-from machine import Pin, PWM
-import time
-import wave
-import Adafruit_Python_CharLCD as LCD
-import board
-import neopixel
+import ujson
 import os
+import wave
+import neopixel
 import urandom
-
-# Function to parse a JSON file
-def parse_json_file(file_path):
-    with open(file_path, 'r') as json_file:
-        data = json.load(json_file)
-    return data
-
-# Load settings from the JSON file
-json_data = parse_json_file('lightsaber_settings.json')
+import board
+import time
+import machine
+# import Adafruit_Python_CharLCD as LCD #Need to find right library
 
 # Initialize GPIO
+GPIO.setmode(GPIO.BCM)
+
+# Define GPIO pins for buttons, speaker, and neopixels
 button1_pin = 9
 button2_pin = 34
 speaker_pin = 11
-
-# Retrieve NeoPixel settings from the json_data dictionary
-neopixel_settings = json_data.get('NeoPixel', {})
-num_pixels = int(neopixel_settings.get('NumPixels', 30))
-neo_pixel_pin = neopixel_settings.get('NeoPixelPin', 'GP10')
+neo_pixel_pin = board.GP10
 
 # Initialize buttons and speaker
 GPIO.setup(button1_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
@@ -38,42 +28,85 @@ pwm_speaker.duty_u16(0)
 # Initialize LCD display
 lcd = LCD.Adafruit_CharLCDPlate()
 
-# Initialize NeoPixel strip
-pixels = neopixel.NeoPixel(board.eval(neo_pixel_pin), num_pixels)
 
-# Function to move up and down in settings
-def move_up():
-    # Your code to move up through settings
-    pass
+# Load settings from lightsaber_settings.json
+with open("lightsaber_settings.json", "r") as settings_file:
+    settings = ujson.load(settings_file)
 
-def move_down():
-    # Your code to move down through settings
-    pass
+# Define the path to the sounds directory
+sounds_directory = "sounds"
 
-# Main loop for lightsaber button actions
+# Function to load sound configurations from a folder
+def load_sound_configs(directory):
+    sound_configs = {}
+    for root, dirs, files in os.walk(directory):
+        for dir_name in dirs:
+            config_file_path = os.path.join(root, dir_name, "config.json")
+            if os.path.exists(config_file_path):
+                with open(config_file_path, "r") as sound_config_file:
+                    sound_configs[dir_name] = ujson.load(sound_config_file)
+    return sound_configs
+
+# Load sound configurations for different sound folders
+sound_configs = load_sound_configs(sounds_directory)
+
+# Initialize and configure the accelerometer
+# (Make sure to add accelerometer initialization code here)
+
+# Initialize and configure NeoPixels
+num_pixels = settings["NeoPixel"]["NumPixels"]
+neo_pixel_pin = board.GP10
+pixels = neopixel.NeoPixel(neo_pixel_pin, num_pixels)
+
+# Define a list of movement thresholds for different effects
+movement_thresholds = {
+    "Swing": 2.0,
+    "Hit": 3.0,
+    # Add more movement types and thresholds as needed
+}
+
+# Define the event loop
+loop = asyncio.get_event_loop()
+
+# Function to detect and respond to movements
+async def detect_movements():
+    while True:
+        # Read accelerometer data
+        # (Add accelerometer data reading code here)
+
+        for movement, threshold in movement_thresholds.items():
+            if accelerometer_data['x'] > threshold:
+                # A swing movement is detected
+                play_random_sound_effect(sound_effects[movement])
+            elif accelerometer_data['y'] > threshold:
+                # A hit movement is detected
+                play_random_sound_effect(sound_effects[movement])
+            # Add more movement detection logic as needed
+
+        await asyncio.sleep(0.1)
+
+# Create a task to detect movements asynchronously
+loop.create_task(detect_movements())
+
+# Main loop for settings management, button actions, and NeoPixel updates
 while True:
-    if GPIO.input(button1_pin) == GPIO.LOW:
-        # Button 1 is pressed, perform action (e.g., toggle on/off)
-        # Check for long press or short press
-        # Update settings as needed
-        pass
+    # Check button 1 state
+    if machine.Pin(9).value() == 0:  # Replace with the actual GPIO pin for button 1
+        # Button 1 is pressed, perform action
+        button1_pressed(None)
 
-    if GPIO.input(button2_pin) == GPIO.LOW:
+    # Check button 2 state
+    if machine.Pin(34).value() == 0:  # Replace with the actual GPIO pin for button 2
         # Button 2 is pressed, perform action
-        # Check for long press or short press
-        # Update settings as needed
-        pass
+        button2_pressed(None)
 
-    # Access settings from json_data dictionary
-    # Modify the behavior of your lightsaber based on the settings
-    # Example: Set NeoPixels based on blade color from JSON file
-    selected_blade_color = json_data.get('Settings', {}).get('BladeColor', 'Blue')
-    colors_section = json_data.get('Colors', {})
-    rgb_color = [int(x) for x in colors_section.get(selected_blade_color, '0,0,0').split(',')]
-    set_neopixel_color(tuple(rgb_color))
+    # Example: Set NeoPixels based on the "Blade Color" setting
+    blade_color = settings["Settings"]["BladeColor"]
+    color_mappings = settings["Colors"]
+    if blade_color in color_mappings:
+        set_neopixel_color([int(x) for x in color_mappings[blade_color].split(",")])
 
-    # Update the LCD display with the current settings
-    # Modify the LCD display behavior based on settings
-    update_lcd_display()
+    await asyncio.sleep(0.1)  # Add a small delay to avoid rapid loop iterations
 
-    time.sleep(0.1)  # Add a small delay to avoid rapid loop iterations
+# Run the event loop
+loop.run_forever()
